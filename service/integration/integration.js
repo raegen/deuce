9d20ada8269d88ee6c760f2b3d5dfbdef97ef42f
Nikola Pavlovic <npavlovic101@gmail.com>
2017-03-19 16:13:23 +0100

    fixed typo in share text

--- service/integration/integration.js
+++ service/integration/integration.js
@@ -6,15 +6,15 @@ define(['facebook'], function (facebook) {
                     facebook.ui({
                         method: 'share',
                         href: 'http://ec2-35-156-124-133.eu-central-1.compute.amazonaws.com/deuce',
-                        title: 'Has reached rank ' + rank + ' in Deuce game',
-                        description: 'Congratulations.',
+                        title: 'I\'ve reached rank ' + rank + ' in Deuce game!',
+                        description: 'Well done.',
                         picture: 'http://i0.kym-cdn.com/entries/icons/facebook/000/000/554/facepalm.jpg'
                     }, callback);
                 }
             },
             twitter: {
                 share: function (rank, callback) {
-                    var text = 'has reached rank ' + rank + ' in Deuce game!',
+                    var text = 'I\ve reached rank ' + rank + ' in Deuce game!',
                         popup;
 
                     popup = window.open('https://twitter.com/intent/tweet?text=' + text);
